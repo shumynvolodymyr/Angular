61389ba6c5b2422bcca392856515cdea87dff0b8
shumynvolodymyr <shumivova@gmail.com>
2021-05-28 22:13:51 +0300

    hw_1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppComponent } from './app.component';
 import { UsersComponent } from './components/users/users.component';
 import {HttpClientModule} from "@angular/common/http";
 import { UserComponent } from './components/user/user.component';
+import { PostComponent } from './components/post/post.component';
+import { PostsComponent } from './components/posts/posts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UsersComponent,
-    UserComponent
+    UserComponent,
+    PostComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule,
